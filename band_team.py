4209e124e685127ce20ece24c34b2e0a43b65ae8
apar2021 <45519735+apar2021@users.noreply.github.com>
2025-02-08 17:09:59 -0500

    Delete band_team.py

--- band_team.py
+++
@@ -1,8 +0,0 @@
-class BandTeam:
-    def __init__(self, team_id, name, location, goals, members, member_count):
-        self.team_id = team_id
-        self.name = name
-        self.location = location
-        self.goals = goals
-        self.members = members
-        self.member_count = member_count
