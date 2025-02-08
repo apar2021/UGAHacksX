72e703df7ba5c2de636fb266463e337c809895e5
apar2021 <45519735+apar2021@users.noreply.github.com>
2025-02-08 02:26:22 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Updated import
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Create a root and render the app
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create root
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Optional: Web Vitals reporting
+reportWebVitals();
---
+++ src/reportWebVitals.js
@@ -0,0 +1,6 @@
+// src/reportWebVitals.js
+const reportWebVitals = (metric) => {
+  console.log(metric); // Log the performance metric (or send it to an analytics endpoint)
+};
+
+export default reportWebVitals;
