5395ab23154876e2cb603e65897a17b1cea16606
apar2021 <45519735+apar2021@users.noreply.github.com>
2025-02-08 19:03:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,11 @@ def index():
 @app.route("/about")
 def about():
     return render_template('about.html')
+@app.route("/artist-register")
+def artist-register():
+    return render_template('artist-register.html')
+@app.route("/band-register")
+def band-register():
+    return render_template('band-register.html')
 if __name__ == "__main__":
     app.run(debug=True)
